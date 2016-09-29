a5271927990eed435ffd1dc1fae82a8b41949fd7
Wilson Giese <giese.wilson@gmail.com>
2016-09-28 21:49:47 -0700

    :)?

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,71 @@
+
+struct Cursor;
+struct MockCursor;
+
+trait CursorType {
+    fn new() -> Self;
+    fn do_something(&self);
+}
+
+impl CursorType for Cursor {
+    fn new() -> Cursor {
+        Cursor { }
+    }
+
+    fn do_something(&self) {
+        panic!()
+    }
+}
+
+impl CursorType for MockCursor {
+    fn new() -> MockCursor {
+        MockCursor { }
+    }
+
+    fn do_something(&self) {
+        println!("It's rude to mock");
+    }
+}
+
+struct Context<Cursor: CursorType> {
+    cursor: Cursor,
+}
+
+impl<C: CursorType> Context<C> {
+    pub fn new(cursor: C) -> Context<C>  {
+        Context {
+            cursor: cursor,
+        }
+    }
+
+    pub fn do_something(&self) {
+        println!("Work");
+        println!("Work");
+        println!("Work");
+        self.cursor.do_something();
+        println!("Work");
+        println!("Work");
+        println!("Work");
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cursor = Cursor::new();
+    let context = Context::new(cursor);
+    context.do_something();
+}
+
+#[test]
+fn test_mock() {
+    let cursor = MockCursor::new();
+    let context = Context::new(cursor);
+    context.do_something();
+}
+
+#[test]
+#[should_panic]
+fn test_real() {
+    let cursor = Cursor::new();
+    let context = Context::new(cursor);
+    context.do_something();
 }
